c950058aee24bcbe611b1e457c19f998cf9fb629
tudor47 <tudorcostin7@gmail.com>
2025-01-25 22:40:02 +0200

    feat: passive perp abi

--- examples/utils/trade.py
+++ examples/utils/trade.py
@@ -143,6 +143,7 @@ def getConfigs():
     core_proxy_address = "0xA763B6a5E09378434406C003daE6487FbbDc1a80" if chain_id == 1729 else "0xC6fB022962e1426F4e0ec9D2F8861c57926E9f72"
     multicall_address = "0xED28d27dFcA47AD2513C9f2e2d3C098C2eA5A47F" if chain_id == 1729 else "0x5abde4F0aF8Eaf3c9967f7fA126E59A103357b5C"
     oracle_adapter_proxy_address = "0x32edABC058C1207fE0Ec5F8557643c28E4FF379e" if chain_id == 1729 else "0xc501A2356703CD351703D68963c6F4136120f7CF"
+    passive_perp_proxy_address = "0x27e5cb712334e101b3c232eb0be198baaa595f5f" if chain_id == 1729 else "0x9ec177fed042ef2307928be2f5cdbf663b20244b"
 
     f = open('examples/abis/CoreProxy.json')
     core_abi = json.load(f)
@@ -153,16 +154,21 @@ def getConfigs():
     f = open('examples/abis/OracleAdapterProxy.json')
     oracle_adapter_abi = json.load(f)
 
+    f = open('examples/abis/PassivePerpProxy.json')
+    passive_perp_abi = json.load(f)
+
     return {
         'core_proxy_address': core_proxy_address,
         'multicall_address': multicall_address,
         'oracle_adapter_proxy_address': oracle_adapter_proxy_address,
+        'passive_perp_proxy_address': passive_perp_proxy_address,
         'private_key': os.environ['PRIVATE_KEY'],
         'pool_id': pool_id,
         'exchange_id': 1,
         'multicall_abi': multicall_abi,
         'oracle_abi': oracle_adapter_abi,
         'core_abi': core_abi,
+        'passive_perp_abi': passive_perp_abi,
         'rpc_url': rpc_url,
         'chain_id': chain_id,
         'account_id': int(os.environ['ACCOUNT_ID'])
