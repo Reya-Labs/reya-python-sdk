19090fe4fac1214db0ebec93ffbc66986688e280
tudor47 <tudorcostin7@gmail.com>
2025-02-13 16:21:45 +0200

    feat: abstracting bridging

--- reya_actions/actions/bridge_in.py
+++ reya_actions/actions/bridge_in.py
@@ -15,16 +15,29 @@ f = open('reya_actions/abis/Erc20.json')
 erc20_abi = json.load(f)
 
 def bridge_in_from_arbitrum(config: dict, params: BridgeInParams):
-    # Ensure Reya Network is configured in configs 
     arbitrum_rpc_url = 'https://arb1.arbitrum.io/rpc'
-    private_key = config['private_key']
+    vault_address = '0x11B3a7E08Eb2FdEa2745e4CB64648b10B28524A8'
+    connector_address = '0xb0d57301050710AF1145562b3386ff5eCFE9BE83'
     chain_id = config['chain_id']
 
+    # Ensure Reya Network is configured in configs
     if not chain_id == 1729:
         raise Exception("Bridging function requires setup for Reya Network")
     
-    # Create w3 instance for Arbitrum using the given private key
-    w3 = Web3(Web3.HTTPProvider(arbitrum_rpc_url))
+    return bridge_in(
+        config=config, 
+        params=params, 
+        chain_rpc_url=arbitrum_rpc_url,
+        vault_address=vault_address,
+        connector_address=connector_address
+    )
+
+def bridge_in(config: dict, params: BridgeInParams, chain_rpc_url: str, vault_address: str, connector_address: str): 
+    # Ensure Reya Network is configured in configs 
+    private_key = config['private_key']
+    
+    # Create w3 instance for the specified chain and private key
+    w3 = Web3(Web3.HTTPProvider(chain_rpc_url))
     w3account = w3.eth.account.from_key(private_key)
     account_address = w3account.address
     w3.eth.default_account = account_address
@@ -33,43 +46,41 @@ def bridge_in_from_arbitrum(config: dict, params: BridgeInParams):
     )
 
     # Get the bridging fee, add 10% buffer and check against the set limit
-    usdc_vault_address = '0x11B3a7E08Eb2FdEa2745e4CB64648b10B28524A8'
-    usdc_connector_address = '0xb0d57301050710AF1145562b3386ff5eCFE9BE83'
     socket_msg_gas_limit = 20_000_000
     socket_empty_payload_size = 160
 
     vault = w3.eth.contract(
-        address=usdc_vault_address, abi=vault_abi
+        address=vault_address, abi=vault_abi
     )
 
-    estimated_socket_fees = vault.functions.getMinFees(usdc_connector_address, socket_msg_gas_limit, socket_empty_payload_size).call()
+    estimated_socket_fees = vault.functions.getMinFees(connector_address, socket_msg_gas_limit, socket_empty_payload_size).call()
     socket_fees = estimated_socket_fees * 110 // 100
 
     if socket_fees > params.fee_limit:
         raise Exception("Socket fee is higher than maximum allowed amount")
 
     # Approve the USDC token to be used by the vault
-    usdc_address = vault.functions.token().call()
-    usdc = w3.eth.contract(
-        address=usdc_address, abi=erc20_abi
+    chain_usdc_address = vault.functions.token().call()
+    chain_usdc = w3.eth.contract(
+        address=chain_usdc_address, abi=erc20_abi
     )
 
-    tx_hash = usdc.functions.approve(usdc_vault_address, params.amount).transact({'from': account_address})
+    tx_hash = chain_usdc.functions.approve(vault_address, params.amount).transact({'from': account_address})
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
     print(f'Approved USDC to vault: ${tx_receipt.transactionHash.hex()}')
 
     # Initiate the bridging transaction
     periphery = config['w3contracts']['periphery']
-    reya_usdc_address = '0x3B860c0b53f2e8bd5264AA7c3451d41263C933F2'
+    reya_usdc = config['w3contracts']['usdc']
     socket_bridge_options = Web3.to_bytes(hexstr='0x')
 
-    periphery_calldata = periphery.encodeABI(fn_name="deposit", args=[(account_address, reya_usdc_address)])
+    periphery_calldata = periphery.encodeABI(fn_name="deposit", args=[(account_address, reya_usdc.address)])
     
     tx_hash = vault.functions.bridge(
         periphery.address,
         params.amount,
         socket_msg_gas_limit,
-        usdc_connector_address,
+        reya_usdc.address,
         periphery_calldata,
         socket_bridge_options
     ).transact({'from': account_address, 'value': socket_fees})
--- reya_actions/actions/bridge_out.py
+++ reya_actions/actions/bridge_out.py
@@ -14,21 +14,30 @@ erc20_abi = json.load(f)
 
 def bridge_out_to_arbitrum(config: dict, params: BridgeOutParams):
     # Ensure Reya Network is configured in configs
+    connector_address = '0x3F19417872BC9F5037Bc0D40cE7389D05Cf847Ad'
+    socket_msg_gas_limit = 20_000_000
+    arbitrum_chain_id = 42161
     chain_id = config['chain_id']
 
     if not chain_id == 1729:
         raise Exception("Bridging function requires setup for Reya Network")
     
+    return bridge_out(
+        config=config,
+        params=params,
+        dest_chain_id=arbitrum_chain_id,
+        connector_address=connector_address,
+        socket_msg_gas_limit=socket_msg_gas_limit
+    )
+
+def bridge_out(config: dict, params: BridgeOutParams, dest_chain_id: int, connector_address: str, socket_msg_gas_limit: int):
     w3 = config['w3']
     account = config['w3account']
     periphery = config['w3contracts']['periphery']
     rusd = config['w3contracts']['rusd']
-    arbitrum_chain_id = 42161
 
     # Get the bridging fee, add 10% buffer and check against the set limit
     controller_address = '0x1d43076909Ca139BFaC4EbB7194518bE3638fc76'
-    connector_address = '0x3F19417872BC9F5037Bc0D40cE7389D05Cf847Ad'
-    socket_msg_gas_limit = 20_000_000
     socket_empty_payload_size = 160
 
     controller = w3.eth.contract(
@@ -51,7 +60,7 @@ def bridge_out_to_arbitrum(config: dict, params: BridgeOutParams):
         params.amount,
         rusd.address,
         socket_msg_gas_limit,
-        arbitrum_chain_id,
+        dest_chain_id,
         account.address,
     )).transact({'from': account.address, 'value': socket_fees})
 
